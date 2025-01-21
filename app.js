16d6fc8fa18c6d24b066cf6c280bd6c232f5cd7e
Minh Pham <phamduya@sheridancollege.ca>
2025-01-20 21:22:15 -0500

    Starting app api with get request

--- app.js
+++ app.js
@@ -1,18 +1,24 @@
 //How to run nodemon: https://stackoverflow.com/questions/35530930/nodemon-not-working-bash-nodemon-command-not-found
 
+const fs = require('fs');
 const express = require('express');
 
 const app = express();
 
-app.get('/', (req, res) => {
-    res.status(200).json({
-        message: 'Hello from the server side!',
-        app: 'Natours'
-    })
-});
+const tours = JSON.parse(
+    fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
 
-app.post('/post', (req, res) => {
-    res.send('You can post on this server')
+app.get('/api/v1/tours', (req, res) => {
+    res
+    .status(200)
+    .json({
+        status: 'success',
+        results: tours.length,
+        data: {
+            tours: tours
+        }
+    })
 });
 
 const port = 3000;
