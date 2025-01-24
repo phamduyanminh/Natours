70b35d31b43f7228975a3f78a5f1b6dfe94543fd
Minh Pham <phamduya@sheridancollege.ca>
2025-01-24 00:32:13 -0500

    Split routes into smaller components

--- app.js
+++ app.js
@@ -12,25 +12,42 @@ const tours = JSON.parse(
     fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
 );
 
-app.get('/api/v1/tours', (req, res) => {
-    res
-    .status(200)
-    .json({
+const getAllTours = (req, res) => {
+    res.status(200).json({
         status: 'success',
         results: tours.length,
         data: {
             tours: tours
         }
     })
-});
+};
 
-app.post('/api/v1/tours', (req, res) => {
+const getTour = (req, res) => {
+    console.log(req.params)
+
+    const id = req.params.id * 1 //Automatically convert string to number value
+    const tour = tours.find(element => element.id === id)
+    if(!tour) return res.status(404).json({
+        status: 'Fail!',
+        message: 'No tour found!'
+    })
+    
+    res.status(200).json({
+        status: 'Retrieve tour success',
+        result: id,
+        data: {
+            tour: tour
+        }
+    })
+};
+
+const createTour = (req, res) => {
     const newId = tours[tours.length-1].id + 1
     const newTour = Object.assign({id: newId}, req.body)
     tours.push(newTour)
     fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(tours), err => {
         res.status(201).json({
-            status: 'success',
+            status: 'Add new tour success',
             data: {
                 tour: newTour
             }
@@ -39,7 +56,45 @@ app.post('/api/v1/tours', (req, res) => {
 
     console.log(newTour)
     console.log('Done with posting')
-});
+};
+
+const updateTour = (req, res) => {
+    console.log(req.params)
+
+    if(req.params.id * 1 > tours.length) return res.status(404).json({
+        status: 'Fail!',
+        message: 'No tour found to update!'
+    })
+
+    res.status(200).json({
+        status: 'Update tour success',
+        data: {
+            tour: '<Updated tour here ...>'
+        }
+    })
+};
+
+const deleteTour = (req, res) => {
+    console.log(req.params)
+
+    if(req.params.id * 1 > tours.length) return res.status(404).json({
+        status: 'Fail!',
+        message: 'No tour found to update!'
+    })
+
+    res.status(200).json({
+        status: 'Delete tour success',
+        data: {
+            tour: '<Deleted tour here ...>'
+        }
+    })
+};
+
+app.get('/api/v1/tours', getAllTours);
+app.get('/api/v1/tours/:id', getTour);
+app.post('/api/v1/tours', createTour);
+app.patch('/api/v1/tours/:id', updateTour);
+app.delete('/api/v1/tours/:id', deleteTour);
 
 const port = 3000;
 app.listen(port, () => {
