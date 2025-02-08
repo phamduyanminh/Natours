71011b4154437586c76bd6a6f7c47a290c06a14e
Minh Pham <phamduya@sheridancollege.ca>
2025-02-07 23:23:46 -0500

    Connect app server to mongodb atlas

--- server.js
+++ server.js
@@ -1,6 +1,20 @@
 const dotenv = require('dotenv');
 dotenv.config({ path: './config.env' }); //Always have to config dotenv before others
+const mongoose = require('mongoose');
 const app = require('./app');
+
+const DB = process.env.DATABASE.replace(
+    '<PASSWORD>',
+    process.env.DATABASE_PASSWORD
+);
+
+mongoose.connect(DB, {
+    useNewUrlParser: true
+}).then(con => {
+    console.log(con.connections);
+    console.log('DB connection successful!');
+}); 
+
 
 // Start server - Port number
 const port = process.env.PORT || 3000;
