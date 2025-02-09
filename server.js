1ed36f5695a660ddf2f7971d53181436ea205d98
Minh Pham <phamduya@sheridancollege.ca>
2025-02-08 22:58:15 -0500

    Create mongoose schema

--- server.js
+++ server.js
@@ -16,6 +16,25 @@ mongoose.connect(DB, {
 }); 
 
 
+// Creating mongoose schema
+const tourSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, 'A tour must have a name!'],
+        unique: true
+    },
+    rating: {
+        type: Number,
+        default: 4.5
+    },
+    price: {
+        type: Number,
+        required: [true, 'A tour must have a price!']
+    },
+});
+
+const Tour = mongoose.model('Tour', tourSchema);
+
 // Start server - Port number
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
