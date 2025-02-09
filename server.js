1bc5e23c06c61a7460ec83866e361ee2341fc448
Minh Pham <phamduya@sheridancollege.ca>
2025-02-08 23:19:04 -0500

    Add first data into mongodb atlas

--- server.js
+++ server.js
@@ -35,6 +35,19 @@ const tourSchema = new mongoose.Schema({
 
 const Tour = mongoose.model('Tour', tourSchema);
 
+const testTour = new Tour({
+    name: 'The Forest Hiker',
+    rating: 4.7,
+    price: 497
+});
+
+testTour.save().then(doc => {
+    console.log(doc);
+}).catch(err => {
+    console.log(`!!! Error: ${err}`);
+});
+
+
 // Start server - Port number
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
