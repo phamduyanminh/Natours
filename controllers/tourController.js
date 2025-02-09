79f6e018e5c9353eea9fc110ddd694cec81c57b2
Minh Pham <phamduya@sheridancollege.ca>
2025-02-09 00:49:40 -0500

    Refactor code to follow MVC design pattern

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,21 +1,20 @@
-const express = require('express');
-const fs = require('fs');
+const Tour = require('../models/tourModel');
 
-const tours = JSON.parse(
-    fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`)
-);
 
-exports.checkID = (req, res, next, val) => {
-    console.log(`Tour id is: ${val}`)
-    if(req.params.id * 1 > tours.length){
-        return res.status(404).json({
-            status: 'Fail!',
-            message: `No tour ${val} found!`
-        })
-    }
-    console.log(`Tour id ${val} is valid`)
-    next()
-};
+
+// We don't have to check Id anymore since we use mongoose
+// For reference, go to tourRoutes.js to see how we call and check Id
+// exports.checkID = (req, res, next, val) => {
+//     console.log(`Tour id is: ${val}`)
+//     if(req.params.id * 1 > tours.length){
+//         return res.status(404).json({
+//             status: 'Fail!',
+//             message: `No tour ${val} found!`
+//         })
+//     }
+//     console.log(`Tour id ${val} is valid`)
+//     next()
+// };
 
 exports.checkBody = (req, res, next) => {
     console.log(`Tour Name: ${req.body.name} - Price: ${req.body.price}`)
@@ -29,35 +28,30 @@ exports.checkBody = (req, res, next) => {
 }
 
 exports.getAllTours = (req, res) => {
-    res.status(200).json({
-        status: 'success',
-        results: tours.length,
-        data: {
-            tours: tours
-        }
-    })
+    // res.status(200).json({
+    //     status: 'success',
+    //     results: tours.length,
+    //     data: {
+    //         tours: tours
+    //     }
+    // })
 };
 
 exports.getTour = (req, res) => {
-    res.status(200).json({
-        status: `Retrieve tour ${req.params.id} success`,
-        data: {
-            tour: tours[req.params.id]
-        }
-    })
+    // res.status(200).json({
+    //     status: `Retrieve tour ${req.params.id} success`,
+    //     data: {
+    //         tour: tours[req.params.id]
+    //     }
+    // })
 };
 
 exports.createTour = (req, res) => {
-    const newId = tours[tours.length-1].id + 1
-    const newTour = Object.assign({id: newId}, req.body)
-    tours.push(newTour)
-    fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(tours), err => {
-        res.status(201).json({
-            status: 'Add new tour success',
-            data: {
-                tour: '<New tour added here ...>'//newTour
-            }
-        })
+    res.status(201).json({
+        status: 'Add new tour success',
+        data: {
+            // tour: '<New tour added here ...>'//newTour
+        }
     })
 
     console.log(newTour)
